a50668187b5eafb8b83778f6d58bb365d2e90fba
Yurii Tsap <famaous@ukr.net>
2015-04-03 17:59:24 +0300

    Some additional functionality

--- app/src/main/java/globallogic/yuriitsap/com/customscrollview/HorizontalScrollView.java
+++ app/src/main/java/globallogic/yuriitsap/com/customscrollview/HorizontalScrollView.java
@@ -2,10 +2,12 @@ package globallogic.yuriitsap.com.customscrollview;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Scroller;
 
 /**
  * Created by yurii.tsap on 4/2/2015.
@@ -15,11 +17,18 @@ public class HorizontalScrollView extends ViewGroup {
     private static final String TAG = "HorizontalScrollView";
     private GestureDetector mGestureDetector;
     private int mMaxChildIndex = 0;
+    private Scroller mScroller;
 
     public HorizontalScrollView(Context context, AttributeSet attrs) {
         super(context, attrs);
 
+        mScroller = new Scroller(getContext());
         mGestureDetector = new GestureDetector(getContext(), new OwnGestureListener());
+    }
+
+    @Override
+    public void scrollBy(int x, int y) {
+        super.scrollBy(x, y);
     }
 
     @Override
@@ -52,6 +61,25 @@ public class HorizontalScrollView extends ViewGroup {
         }
     }
 
+    private void showEvent(MotionEvent event) {
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                Log.e(TAG, "ACTION_DOWN x = " + event.getX());
+                break;
+            case MotionEvent.ACTION_MOVE:
+                Log.e(TAG, "ACTION_MOVE x = " + event.getX());
+                break;
+            case MotionEvent.ACTION_UP:
+                Log.e(TAG, "ACTION_UP x = " + event.getX());
+                break;
+            case MotionEvent.ACTION_CANCEL:
+                Log.e(TAG, "ACTION_UP x = " + event.getX());
+                break;
+
+        }
+
+    }
+
     @Override
     public boolean onInterceptTouchEvent(MotionEvent ev) {
         return mGestureDetector.onTouchEvent(ev);
@@ -59,34 +87,82 @@ public class HorizontalScrollView extends ViewGroup {
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
-        return mGestureDetector.onTouchEvent(event);
+        mGestureDetector.onTouchEvent(event);
+        return true;
     }
 
     private class OwnGestureListener extends GestureDetector.SimpleOnGestureListener {
 
+        private boolean parentInterceptionAllowed;
+
         @Override
         public boolean onDown(MotionEvent e) {
-            return !touchInChild(e.getX(), e.getY());
+            parentInterceptionAllowed = true;
+            return false;
         }
+
+        private void disableVerticalScrolling() {
+            if (parentInterceptionAllowed) {
+                requestDisallowInterceptTouchEvent(true);
+                parentInterceptionAllowed = false;
+            }
+
+        }
+
 
         @Override
         public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
-            requestDisallowInterceptTouchEvent(true);
-            int offset = (int) (e2.getX() - e2.getX() < 0 ? distanceX : -distanceX);
-            if (canScroll(offset)) {
+            disableVerticalScrolling();
+            if (canScroll(-distanceX)) {
                 for (int i = getChildCount() - 1; i >= 0; i--) {
-                    getChildAt(i).offsetLeftAndRight(offset);
+                    getChildAt(i).offsetLeftAndRight((int) -distanceX);
                 }
             }
+            Log.e(TAG,"Last child X = "+getChildAt(getChildCount()-1).getRight());
+            invalidate();
             return true;
         }
 
-        private boolean canScroll(int offset) {
+        private boolean leftBorderIsReached() {
+            return getChildAt(0).getLeft() == getPaddingLeft();
+        }
+
+        private boolean rightBorderIsReached() {
+            return getChildAt(getChildCount() - 1).getRight()
+                    == (getRight() - getLeft()) - getPaddingRight();
+        }
+
+        private void scrollToEnd() {
+            if (!rightBorderIsReached()) {
+                Log.e(TAG, "scrollToEnd");
+                for (int i = getChildCount() - 1; i >= 0; i--) {
+                    getChildAt(i).offsetLeftAndRight(
+                            getPaddingRight() - getChildAt(getChildCount() - 1).getRight());
+                }
+            }
+
+
+        }
+
+        private void scrollToStart() {
+            if (!leftBorderIsReached()) {
+                Log.e(TAG, "scrollToStart");
+                for (int i = getChildCount() - 1; i >= 0; i--) {
+                    getChildAt(i).offsetLeftAndRight(Math.abs(getChildAt(0).getLeft()));
+                }
+            }
+        }
+
+
+        private boolean canScroll(float offset) {
+
             return getChildAt(0).getLeft() + offset < getPaddingLeft()
                     && getChildAt(getChildCount() - 1).getRight() + offset
                     > (getRight() - getLeft()) - getPaddingRight();
         }
 
+
+        @Deprecated
         private boolean touchInChild(float x, float y) {
             return ((x > getChildAt(0).getLeft() && x < getChildAt(getChildCount() - 1).getRight())
                     && y > getChildAt(mMaxChildIndex).getTop() && y < getChildAt(mMaxChildIndex)
--- app/src/main/java/globallogic/yuriitsap/com/customscrollview/MainActivity.java
+++ app/src/main/java/globallogic/yuriitsap/com/customscrollview/MainActivity.java
@@ -3,19 +3,36 @@ package globallogic.yuriitsap.com.customscrollview;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 
 public class MainActivity extends ActionBarActivity {
 
+    private HorizontalScrollView mHorizontalScrollView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+        mHorizontalScrollView = (HorizontalScrollView) findViewById(R.id.horizontal_scroller);
+        for (int i = 0; i < 100; i++) {
+            Button button = new Button(MainActivity.this);
+            button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    showToast(v);
+                }
+            });
+            button.setText("Button N" + i);
+            mHorizontalScrollView.addView(button);
+        }
     }
 
     public void showToast(View view) {
-        Toast.makeText(MainActivity.this, "Your answer is correct!",
+        Toast.makeText(MainActivity.this,
+                "View left " + view.getLeft() + " right " + view.getRight(),
                 Toast.LENGTH_SHORT).
                 show();
     }
